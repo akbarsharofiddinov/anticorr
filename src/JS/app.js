bd43df1d9552d51d71795b106cc11cf25f087eb4
akbarsharofiddinov <akbarjonsharofiddinov2004@gmail.com>
2024-08-09 20:08:05 +0500

    Sms varification modal done.

--- src/JS/app.js
+++ src/JS/app.js
@@ -29,3 +29,43 @@ textarea.addEventListener("keyup", (e) => {
 
   word_counter.innerHTML = `${count}/${maxLength}`;
 });
+
+// Custon Select Element
+document
+  .querySelector(".select-selected")
+  .addEventListener("click", function () {
+    this.nextElementSibling.classList.toggle("show");
+  });
+
+document.querySelectorAll(".select-items div").forEach(function (item) {
+  item.addEventListener("click", function () {
+    const selected = document.querySelector(".select-selected");
+    selected.textContent = this.textContent;
+    document.querySelector(".select-items").classList.remove("show");
+  });
+});
+
+document.addEventListener("click", function (e) {
+  if (!e.target.closest(".custom-select")) {
+    document.querySelector(".select-items").classList.remove("show");
+  }
+});
+
+// Modal Opening handle
+
+const sendButton = document.querySelector(".button-submit");
+const modal = document.querySelector(".modal");
+const modalInner = document.querySelector(".modal-inner");
+
+sendButton.addEventListener("click", (e) => {
+  console.log("Send button");
+  e.preventDefault();
+  modal.classList.add("active");
+});
+
+modal.addEventListener("click", (e) => {
+  modal.classList.remove("active");
+  modalInner.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+});
